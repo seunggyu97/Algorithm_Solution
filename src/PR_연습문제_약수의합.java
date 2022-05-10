fb803fc40d0aaa540ef0e931e966868e9bfec2bb
seunggyu <seunggyu97@gmail.com>
2022-05-10 20:47:43 +0900

    5/10 문제풀이

---
+++ src/PR_연습문제_약수의합.java
@@ -0,0 +1,18 @@
+//class Solution {
+//    public int solution(int n){
+//        int answer = n;
+//        for(int i = 1; i<n/2+1; i++){
+//            if(n%i == 0){
+//                answer += i;
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    public static void main(String[] args){
+//        int n = 12;
+//        Solution sol = new Solution();
+//
+//        System.out.println(sol.solution(n));
+//    }
+//}
--- src/PR_연습문제_직사각형별찍기.java
+++ src/PR_연습문제_직사각형별찍기.java
@@ -1,28 +1,28 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
-
-class Solution{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        for(int i = 0; i<m; i++){
-            if(i != 0){
-                sb.append("\n");
-            }
-            sb.append("*".repeat(n));
-        }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
-}
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+//import java.io.IOException;
+//import java.util.StringTokenizer;
+//
+//class Solution{
+//    public static void main(String[] args) throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        StringBuilder sb = new StringBuilder();
+//
+//        int n = Integer.parseInt(st.nextToken());
+//        int m = Integer.parseInt(st.nextToken());
+//
+//        for(int i = 0; i<m; i++){
+//            if(i != 0){
+//                sb.append("\n");
+//            }
+//            sb.append("*".repeat(n));
+//        }
+//        bw.write(sb.toString());
+//        bw.flush();
+//        bw.close();
+//    }
+//}
