cd7939cca3229acd44ebec497e426a2513078cba
seunggyu <seunggyu97@gmail.com>
2022-06-14 19:26:01 +0900

    6/14 문제풀이

---
+++ src/PR_월간코드챌린지시즌3_나머지가1이되는수찾기.java
@@ -0,0 +1,19 @@
+//class Solution{
+//    public int solution(int n){
+//        int answer = 0;
+//        for(int i = 2; i<n;i++){
+//            if(n%i == 1){
+//                answer = i;
+//                break;
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    public static void main(String[] args){
+//        int n = 5;
+//        Solution sol = new Solution();
+//
+//        System.out.println(sol.solution(n));
+//    }
+//}
