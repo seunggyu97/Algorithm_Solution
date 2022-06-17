35513b11aafad72dbb3650f990d1d98109e4948d
seunggyu <seunggyu97@gmail.com>
2022-06-17 23:35:19 +0900

    6/17 문제풀이

---
+++ src/PR_월간코드챌린지시즌2_약수의개수와덧셈.java
@@ -0,0 +1,22 @@
+class Solution{
+    public int solution(int left, int right){
+        int count = 0;
+
+        for(int i = left; i< right+1; i++){
+            int tmp = 0;
+            for(int j = 1; j<=i; j ++){
+                if(i%j == 0) tmp ++;
+            }
+            if(tmp % 2 == 0) count += i;
+            else count -= i;
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        int left = 13;
+        int right = 17;
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(left, right));
+    }
+}
