a08c459b2c08d0e2594b05e0f9732f82f697739c
seunggyu <seunggyu97@gmail.com>
2022-06-24 03:59:16 +0900

    6/24 문제풀이

--- src/PR_2019KakaoBlindRecruitment_실패율.java
+++ src/PR_2019KakaoBlindRecruitment_실패율.java
@@ -1,71 +1,71 @@
-import java.util.*;
-
-class Solution{
-    class Fail{
-        int stage;
-        double rate;
-        Fail(int stage, double rate){
-            this.stage = stage;
-            this.rate = rate;
-        }
-    }
-
-    Comparator<Fail> comp = new Comparator<Fail>() {
-        @Override
-        public int compare(Fail o1, Fail o2) {
-            if(o1.rate < o2.rate){
-                return 1;
-            }else if(o1.rate>o2.rate){
-                return -1;
-            }else{
-                if(o1.stage > o2.stage){
-                    return 1;
-                }else if(o1.stage < o2.stage){
-                    return -1;
-                }else{
-                    return 0;
-                }
-            }
-        }
-    };
-
-    public int[] solution(int N, int[] stages){
-        // N : 스테이지 수
-        // stages : 현재 머물러있는 스테이지 배열
-        // (N+1은 마지막 스테이지를 클리어 한 사용자)
-        // 실패율 : 스테이지에 도달했지만 아직 클리어하지 못한 플레이어 수 / 스테이지에 도달한 플레이어 수
-        // 실패율이 같다면 작은 번호의 스테이지가 먼저 옴
-        // 스테이지에 도달한 플레이어가 없는 경우 해당 스테이지의 실패율은 0
-        // 반환 : 실패율이 높은 순서부터 내림차순으로 담은 배열
-        int[] answer = new int[N];
-        List<Fail> fails = new ArrayList<Fail>();
-        int total = stages.length;
-
-        int[] users = new int[N+1];
-        for(int i : stages) users[i-1]++;
-        for(int i = 0; i<N; i++){
-            if(users[i]==0){
-                fails.add(new Fail(i+1,0));
-            }
-            else{
-                fails.add(new Fail(i+1, (double)users[i]/total));
-                total -= users[i];
-            }
-        }
-
-        Collections.sort(fails,comp);
-        for(int i = 0; i<N; ++i){
-            answer[i] = fails.get(i).stage;
-        }
-
-        return answer;
-    }
-
-    public static void main(String[] args){
-        int N = 5;
-        int[] stages = {2,1,2,6,2,4,3,3};
-        Solution sol = new Solution();
-
-        System.out.println(Arrays.toString(sol.solution(N, stages)));
-    }
-}
+//import java.util.*;
+//
+//class Solution{
+//    class Fail{
+//        int stage;
+//        double rate;
+//        Fail(int stage, double rate){
+//            this.stage = stage;
+//            this.rate = rate;
+//        }
+//    }
+//
+//    Comparator<Fail> comp = new Comparator<Fail>() {
+//        @Override
+//        public int compare(Fail o1, Fail o2) {
+//            if(o1.rate < o2.rate){
+//                return 1;
+//            }else if(o1.rate>o2.rate){
+//                return -1;
+//            }else{
+//                if(o1.stage > o2.stage){
+//                    return 1;
+//                }else if(o1.stage < o2.stage){
+//                    return -1;
+//                }else{
+//                    return 0;
+//                }
+//            }
+//        }
+//    };
+//
+//    public int[] solution(int N, int[] stages){
+//        // N : 스테이지 수
+//        // stages : 현재 머물러있는 스테이지 배열
+//        // (N+1은 마지막 스테이지를 클리어 한 사용자)
+//        // 실패율 : 스테이지에 도달했지만 아직 클리어하지 못한 플레이어 수 / 스테이지에 도달한 플레이어 수
+//        // 실패율이 같다면 작은 번호의 스테이지가 먼저 옴
+//        // 스테이지에 도달한 플레이어가 없는 경우 해당 스테이지의 실패율은 0
+//        // 반환 : 실패율이 높은 순서부터 내림차순으로 담은 배열
+//        int[] answer = new int[N];
+//        List<Fail> fails = new ArrayList<Fail>();
+//        int total = stages.length;
+//
+//        int[] users = new int[N+1];
+//        for(int i : stages) users[i-1]++;
+//        for(int i = 0; i<N; i++){
+//            if(users[i]==0){
+//                fails.add(new Fail(i+1,0));
+//            }
+//            else{
+//                fails.add(new Fail(i+1, (double)users[i]/total));
+//                total -= users[i];
+//            }
+//        }
+//
+//        Collections.sort(fails,comp);
+//        for(int i = 0; i<N; ++i){
+//            answer[i] = fails.get(i).stage;
+//        }
+//
+//        return answer;
+//    }
+//
+//    public static void main(String[] args){
+//        int N = 5;
+//        int[] stages = {2,1,2,6,2,4,3,3};
+//        Solution sol = new Solution();
+//
+//        System.out.println(Arrays.toString(sol.solution(N, stages)));
+//    }
+//}
---
+++ src/PR_2022KakaoBlindRecruitment_신고결과받기.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution{
+    public int[] solution(String[] id_list, String[] report, int k){
+        // 기본 베이스 구축
+        HashMap<String, ArrayList<String>> hm = new HashMap<>();
+        HashMap<String, Integer> resultMap = new HashMap<>();
+        for(String s : id_list){
+            hm.put(s, new ArrayList<>());
+            resultMap.put(s, 0);
+        }
+
+        Loop1 : for(String r : report){
+            String[] reportArr = r.split(" ");
+            ArrayList<String> tmpList = hm.get(reportArr[1]);
+            if(tmpList.isEmpty()){
+                tmpList.add(reportArr[0]);
+                hm.put(reportArr[1], tmpList);
+                continue;
+            }
+            for (String s : tmpList) {
+                if (reportArr[0].equals(s)) {
+                    continue Loop1;
+                }
+            }
+            tmpList.add(reportArr[0]);
+        }
+
+        // 신고 결과 처리
+        int[] result = new int[id_list.length];
+
+        for(Map.Entry<String, ArrayList<String>> entry : hm.entrySet()){
+            if(entry.getValue().size() >= k){
+                for(String s : entry.getValue()){
+                    resultMap.put(s,resultMap.getOrDefault(s,0)+1);
+                }
+            }
+        }
+        for(int i = 0; i< id_list.length; i++){
+            result[i] = resultMap.get(id_list[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+        String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+        String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
+        int k = 2;
+
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution(id_list, report, k)));
+    }
+}
