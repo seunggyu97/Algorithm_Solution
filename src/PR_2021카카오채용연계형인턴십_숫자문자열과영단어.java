fb7f83d0e7a69b2787f8a728172d2a2a257d00f6
seunggyu <seunggyu97@gmail.com>
2022-06-25 04:55:08 +0900

    6/25 문제풀이

---
+++ src/PR_2021카카오채용연계형인턴십_숫자문자열과영단어.java
@@ -0,0 +1,27 @@
+class Solution{
+    public int solution(String s) {
+        String[] word = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<s.length(); ){
+            if(s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+                sb.append(s.charAt(i++));
+            }else{
+                for(int j = 0; j<10; j++){
+                    if(s.startsWith(word[j],i)){
+                        sb.append(j);
+                        i += word[j].length();
+                        break;
+                    }
+                }
+            }
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+    public static void main(String[] args){
+        String s = "one4seveneight";
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(s));
+    }
+}
