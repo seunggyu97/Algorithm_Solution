927c10bac386f464f6ecfaa94a2ecd5d6828693d
seunggyu <seunggyu97@gmail.com>
2022-06-26 16:33:10 +0900

    6/26 문제풀이

---
+++ src/Kotlin/PR_연습문재_x만큼간격이있는n개의숫자.kt
@@ -0,0 +1,11 @@
+package Kotlin
+
+import java.util.*
+
+class Solution{
+    fun solution(x:Int, n:Int): LongArray = LongArray(n){x.toLong() * (it+1)}
+}
+fun main(){
+    val sol = Solution()
+    println(sol.solution(3, 5).contentToString())
+}
