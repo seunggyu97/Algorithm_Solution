25e5f9cc519b1987e62cce60ff808deeaf1f2a30
seunggyu <seunggyu97@gmail.com>
2022-07-01 13:15:06 +0900

    7/1 문제풀이

--- src/Kotlin/PR_2021DevMatching_로또의최고순위와최저순위.kt
+++ src/Kotlin/PR_2021DevMatching_로또의최고순위와최저순위.kt
@@ -1,28 +1,28 @@
-package Kotlin
-
-class Solution {
-    fun solution(lottos : IntArray, win_nums : IntArray):IntArray{
-        var zero = lottos.filter { it == 0 }.size;
-        var low = 7
-        var high = 7
-
-        lottos.forEach { num ->
-            if(win_nums.any{lottoNum -> lottoNum == num}){
-                low--
-                high--
-            }
-        }
-        high -= zero
-
-        if(high > 6) high = 6
-        if(low > 6) low = 6
-        return intArrayOf(high,low)
-    }
-}
-fun main(){
-    val lottos = intArrayOf(44, 1, 0, 0, 31, 25)
-    val win_nums = intArrayOf(31, 10, 45, 1, 6, 19)
-
-    val solution = Solution()
-    println(solution.solution(lottos, win_nums).contentToString())
-}
+//package Kotlin
+//
+//class Solution {
+//    fun solution(lottos : IntArray, win_nums : IntArray):IntArray{
+//        var zero = lottos.filter { it == 0 }.size;
+//        var low = 7
+//        var high = 7
+//
+//        lottos.forEach { num ->
+//            if(win_nums.any{lottoNum -> lottoNum == num}){
+//                low--
+//                high--
+//            }
+//        }
+//        high -= zero
+//
+//        if(high > 6) high = 6
+//        if(low > 6) low = 6
+//        return intArrayOf(high,low)
+//    }
+//}
+//fun main(){
+//    val lottos = intArrayOf(44, 1, 0, 0, 31, 25)
+//    val win_nums = intArrayOf(31, 10, 45, 1, 6, 19)
+//
+//    val solution = Solution()
+//    println(solution.solution(lottos, win_nums).contentToString())
+//}
