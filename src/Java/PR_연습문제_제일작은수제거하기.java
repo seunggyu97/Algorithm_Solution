de588be9817daefd4e2f5e3bbf43dee266f1d366
seunggyu <seunggyu97@gmail.com>
2022-07-02 18:18:25 +0900

    7/2 문제풀이

---
+++ src/Java/PR_연습문제_제일작은수제거하기.java
@@ -0,0 +1,37 @@
+package Java;
+
+import java.util.Arrays;
+
+class Solution{
+    public int[] solution(int[] arr){
+        if(arr.length == 1){
+            return new int[]{-1};
+        }
+
+        int min = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] < arr[min]) min = i;
+        }
+
+        int[] answer = new int[arr.length-1];
+        boolean removed = false;
+        for(int i = 0; i<arr.length; i++){
+            if(removed){
+                answer[i-1] = arr[i];
+                continue;
+            }
+            if(i == min){
+                removed = true;
+                continue;
+            }
+            answer[i] = arr[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {4,1,2,3};
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.solution(arr)));
+    }
+}
