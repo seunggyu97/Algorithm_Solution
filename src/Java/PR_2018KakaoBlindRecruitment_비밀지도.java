982d22e8f334bbb7eef2e4070496c7f3eb53d29e
seunggyu <seunggyu97@gmail.com>
2022-07-04 21:51:51 +0900

    7/4 문제풀이

--- src/Java/PR_2018KakaoBlindRecruitment_비밀지도.java
+++ src/Java/PR_2018KakaoBlindRecruitment_비밀지도.java
@@ -1,69 +1,69 @@
-package Java;
-
-import java.util.Arrays;
-
-class Solution{
-    int maxLength = 0;
-    StringBuilder sb;
-    public String[] solution(int n, int[] arr1, int[] arr2){
-        String[] answer = new String[n];
-
-        for(int i = 0; i < n; i++){
-            answer[i] = compare(arr1[i],arr2[i]);
-        }
-
-        for(int i = 0; i < n; i++){
-            if(answer[i].length() < maxLength){
-                int count = maxLength - answer[i].length();
-                String tmp = "";
-                for(int j = 0; j < count; j++){
-                    tmp += " ";
-                }
-                answer[i] = tmp + answer[i];
-            }
-        }
-        return answer;
-    }
-
-    public String compare(int num1, int num2){
-        sb = new StringBuilder();
-        String convert1 = Integer.toBinaryString(num1);
-        String convert2 = Integer.toBinaryString(num2);
-        int tmpLength = convert1.length();
-        if(convert1.length() < convert2.length()){
-            String tmp = "";
-            tmpLength = convert2.length();
-            int count = convert2.length() - convert1.length();
-            for(int i = 0; i<count; i++){
-                tmp += "0";
-            }
-            convert1 = tmp + convert1;
-        }
-        else if (convert2.length() < convert1.length()){
-            String tmp = "";
-            int count = convert1.length() - convert2.length();
-            for(int i = 0; i<count; i++){
-                tmp += "0";
-            }
-            convert2 = tmp + convert2;
-        }
-        for(int i = 0; i<convert1.length(); i++){
-            if (Character.getNumericValue(convert1.charAt(i)) + Character.getNumericValue(convert2.charAt(i)) == 0) {
-                sb.append(" ");
-            } else {
-                sb.append("#");
-            }
-        }
-        if(maxLength < tmpLength) maxLength = tmpLength;
-
-        return sb.toString();
-    }
-    public static void main(String[] args){
-        int n = 5;
-        int[] arr1 = {46,33,33,22,31,50};
-        int[] arr2 = {27,56,19,14,14,10};
-
-        Solution sol = new Solution();
-        System.out.println(Arrays.toString(sol.solution(n, arr1, arr2)));
-    }
-}
+//package Java;
+//
+//import java.util.Arrays;
+//
+//class Solution{
+//    int maxLength = 0;
+//    StringBuilder sb;
+//    public String[] solution(int n, int[] arr1, int[] arr2){
+//        String[] answer = new String[n];
+//
+//        for(int i = 0; i < n; i++){
+//            answer[i] = compare(arr1[i],arr2[i]);
+//        }
+//
+//        for(int i = 0; i < n; i++){
+//            if(answer[i].length() < maxLength){
+//                int count = maxLength - answer[i].length();
+//                String tmp = "";
+//                for(int j = 0; j < count; j++){
+//                    tmp += " ";
+//                }
+//                answer[i] = tmp + answer[i];
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    public String compare(int num1, int num2){
+//        sb = new StringBuilder();
+//        String convert1 = Integer.toBinaryString(num1);
+//        String convert2 = Integer.toBinaryString(num2);
+//        int tmpLength = convert1.length();
+//        if(convert1.length() < convert2.length()){
+//            String tmp = "";
+//            tmpLength = convert2.length();
+//            int count = convert2.length() - convert1.length();
+//            for(int i = 0; i<count; i++){
+//                tmp += "0";
+//            }
+//            convert1 = tmp + convert1;
+//        }
+//        else if (convert2.length() < convert1.length()){
+//            String tmp = "";
+//            int count = convert1.length() - convert2.length();
+//            for(int i = 0; i<count; i++){
+//                tmp += "0";
+//            }
+//            convert2 = tmp + convert2;
+//        }
+//        for(int i = 0; i<convert1.length(); i++){
+//            if (Character.getNumericValue(convert1.charAt(i)) + Character.getNumericValue(convert2.charAt(i)) == 0) {
+//                sb.append(" ");
+//            } else {
+//                sb.append("#");
+//            }
+//        }
+//        if(maxLength < tmpLength) maxLength = tmpLength;
+//
+//        return sb.toString();
+//    }
+//    public static void main(String[] args){
+//        int n = 5;
+//        int[] arr1 = {46,33,33,22,31,50};
+//        int[] arr2 = {27,56,19,14,14,10};
+//
+//        Solution sol = new Solution();
+//        System.out.println(Arrays.toString(sol.solution(n, arr1, arr2)));
+//    }
+//}
---
+++ src/Java/PR_2019카카오개발자겨울인턴십_크레인인형뽑기게임.java
@@ -0,0 +1,45 @@
+package Java;
+
+import java.util.Stack;
+
+class Solution{
+    public int solution(int[][] board, int[] moves){
+        int result = 0;
+        Stack<Integer> basket = new Stack<>();
+        Stack<Integer>[] stack = new Stack[board[0].length];
+        for(int i=0; i<board[0].length; i++){
+            stack[i] = new Stack<Integer>();
+        }
+        for(int i = board.length-1; i>=0; i--){
+            for(int j = 0; j<board[0].length; j++){
+
+                if(board[i][j] != 0) {
+                    stack[j].push(board[i][j]);
+                }
+            }
+        }
+        for(int i : moves){
+
+            if(stack[i-1].isEmpty()) continue;
+            int tmp = -1;
+            if(!basket.isEmpty()) tmp = basket.peek();
+            if (tmp != stack[i - 1].peek()) {
+                basket.push(stack[i - 1].pop());
+            } else {
+                basket.pop();
+                stack[i - 1].pop();
+                result += 2;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args){
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+
+        Solution sol = new Solution();
+        System.out.println(sol.solution(board, moves));
+    }
+}
