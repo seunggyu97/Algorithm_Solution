45275bbe9faf844fb48082a1f4b234de8d1315ad
seunggyu <seunggyu97@gmail.com>
2022-07-06 23:18:17 +0900

    7/6 문제풀이

---
+++ src/Java/PR_연습문제_최대공약수와최소공배수.java
@@ -0,0 +1,29 @@
+package Java;
+
+import java.util.Arrays;
+
+class Solution{
+    public long[] solution(int n, int m){
+        long[] answer = new long[2];
+
+        if(m%n == 0){
+            answer[0] = n;
+            answer[1] = m;
+        }else{
+            for(int i = 1; i<=n; i++){
+                if(n%i == 0 && m%i ==0){
+                    answer[0] = i;
+                }
+            }
+            answer[1] = n*m/answer[0];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        int n = 3, m = 12;
+        Solution sol = new Solution();
+
+        System.out.println(Arrays.toString(sol.solution(n,m)));
+    }
+}
