3d6825b2b0c43444c6df7da18be73aecc556e551
seunggyu <seunggyu97@gmail.com>
2022-07-11 23:57:02 +0900

    7/11 문제풀이

---
+++ src/Java/Programmers/Lv2/PR_연습문제_땅따먹기.java
@@ -0,0 +1,34 @@
+package Java.Programmers.Lv2;
+
+class Dp{
+    int dp(int[][] land) {
+        final int length = land.length;
+
+        for(int i=1; i<length; i++) {
+            land[i][0] += max(land[i-1][1], land[i-1][2], land[i-1][3]);
+            land[i][1] += max(land[i-1][0], land[i-1][2], land[i-1][3]);
+            land[i][2] += max(land[i-1][1], land[i-1][3], land[i-1][0]);
+            land[i][3] += max(land[i-1][1], land[i-1][2], land[i-1][0]);
+        }
+
+        return max(land[length-1]);
+    }
+
+    public int max(int a, int b, int c) {
+        return Math.max(Math.max(a, b), c);
+    }
+
+    public int max(int[] arr) {
+        int max = 0;
+        for(int number : arr) {
+            max = Math.max(max, number);
+        }
+        return max;
+    }
+
+    public static void main(String[] args){
+        int[][] land = {{1,2,102,100},{100,55,150,95},{4,3,2,1}};
+        Dp sol = new Dp();
+        System.out.println(sol.dp(land));
+    }
+}
