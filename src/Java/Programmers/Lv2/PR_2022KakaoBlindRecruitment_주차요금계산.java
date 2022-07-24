933135e3a6c04f5144b2ef68f2ed6911b97db3ca
seunggyu <seunggyu97@gmail.com>
2022-07-25 02:53:10 +0900

    7/25 문제풀이

--- src/Java/Programmers/Lv2/PR_2022KakaoBlindRecruitment_주차요금계산.java
+++ src/Java/Programmers/Lv2/PR_2022KakaoBlindRecruitment_주차요금계산.java
@@ -13,38 +13,43 @@ class ParkingFee{
                 hm.put(arr[1], arr[0]);
             }
             else{
-                String time = hm.get(arr[1]);
-                String[] timeOutArr = time.split(":");
-                String[] timeInArr = arr[0].split(":");
+                String[] timeInArr = hm.get(arr[1]).split(":");
+                String[] timeOutArr = arr[0].split(":");
                 int timeIn = Integer.parseInt(timeInArr[0])*60 + Integer.parseInt(timeInArr[1]);
                 int timeOut = Integer.parseInt(timeOutArr[0])*60 + Integer.parseInt(timeOutArr[1]);
-                accuredTime.put(arr[1], timeOut-timeIn);
-
+                accuredTime.put(arr[1], accuredTime.getOrDefault(arr[1],0)+timeOut-timeIn);
                 // in - out 처리 완료되면 해시맵에서 삭제
                 hm.remove(arr[1]);
             }
         }
+
+        // 출차 하지 않은 차량 요금계산
         if(!hm.isEmpty()) {
             for (Map.Entry<String, String> entry : hm.entrySet()) {
                 String[] time = entry.getValue().split(":");
                 int resultTime = (60*23 + 59) - ((Integer.parseInt(time[0])*60)+ Integer.parseInt(time[1]));
-                accuredTime.put(entry.getKey(), Integer.parseInt(entry.getValue())+resultTime);
+                accuredTime.put(entry.getKey(), accuredTime.getOrDefault(entry.getKey(),0)+resultTime);
             }
         }
         int[] result = new int[accuredTime.size()];
-        int idx = 0;
-        for(Map.Entry<String, Integer> entry : accuredTime.entrySet()){
-            if(entry.getValue() > ){
-
+        List<Map.Entry<String, Integer>> list = new ArrayList(accuredTime.entrySet());
+        list.sort((o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.getKey()), Integer.parseInt(o2.getKey()));
+        });
+        for(int i = 0; i<list.size(); i++){
+            if(list.get(i).getValue() <= fees[0]){
+                result[i] = fees[1];
+            }else{
+                result[i] = fees[1] + (int)Math.ceil((double)(list.get(i).getValue() - fees[0])/fees[2])*fees[3];
             }
-
-            idx++;
         }
+
+        return result;
     }
 
     public static void main(String[] args){
         int[] fees = {180, 5000, 10, 600};
-        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+        String[] records = {"05:34 5961 IN", "06:34 5961 OUT", "07:34 5961 IN", "08:34 5961 OUT", "09:34 5961 IN", "10:34 5961 OUT", "11:34 5961 IN", "12:34 5961 OUT"};
 
         ParkingFee sol = new ParkingFee();
         System.out.println(Arrays.toString(sol.parkingFee(fees, records)));
