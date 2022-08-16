30f4ad65e5c97b285e26aac65e245d65283a7c48
seunggyu <seunggyu97@gmail.com>
2022-08-16 21:15:18 +0900

    8/16 문제풀이

---
+++ src/Java/Baekjoon/BJ_Silver3_14425_문자열집합.java
@@ -0,0 +1,26 @@
+package Java.Baekjoon;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int answer = 0;
+
+        HashSet<String> hs = new HashSet<>();
+        for(int i = 0; i < N; i++){
+            hs.add(br.readLine());
+        }
+        for(int i = 0; i < M; i++){
+            if(hs.contains(br.readLine())) answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
