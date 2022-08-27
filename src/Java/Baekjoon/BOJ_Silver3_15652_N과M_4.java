d56b741ec05386b36b4e167bf1e55b2ec6373468
seunggyu <seunggyu97@gmail.com>
2022-08-27 23:11:39 +0900

    8/27 문제풀이

---
+++ src/Java/Baekjoon/BOJ_Silver3_15652_N과M_4.java
@@ -0,0 +1,42 @@
+package Java.Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class NAndM_4{
+    public static int[] arr;
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth, int at) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = at; i <= N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1, i);
+        }
+    }
+}
