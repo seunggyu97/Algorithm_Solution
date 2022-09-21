d36e8c96ea192d16174db3e116bf87e7555a61f8
seunggyu <seunggyu97@gmail.com>
2022-09-21 13:55:36 +0900

    9/21 문제풀이

---
+++ src/Java/Programmers/Lv2/PR_연습문제_다음큰숫자.java
@@ -0,0 +1,27 @@
+package Java.Programmers.Lv2;
+
+class NextBigNum{
+    public int nextBigNum(int n){
+        int count = checkNum(n);
+        for(int i = n + 1;;i++){
+            if(checkNum(i) == count) return i;
+        }
+    }
+
+    public int checkNum(int n){
+        int count = 0;
+        String binary = Integer.toBinaryString(n);
+        for(int i = 0; i < binary.length(); i++){
+            if(binary.charAt(i) == '1') count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args){
+        int n = 78;
+        NextBigNum sol = new NextBigNum();
+
+        System.out.println(sol.nextBigNum(n));
+    }
+}
