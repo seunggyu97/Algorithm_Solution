3704af6fc885b2d679b3865bee94cef348e08f2e
seunggyu <seunggyu97@gmail.com>
2022-11-04 16:45:53 +0900

    11/4 문제풀이 백트래킹 복습

--- src/Java/Baekjoon/BOJ_Silver3_15650_N과M_2.java
+++ src/Java/Baekjoon/BOJ_Silver3_15650_N과M_2.java
@@ -1,42 +1,39 @@
 package Java.Baekjoon;
 
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.BufferedReader;
 import java.util.StringTokenizer;
+import java.io.IOException;
 
-class NAndM_2 {
-
-    public static int[] arr;
-    public static StringBuilder sb = new StringBuilder();
-    static int N, M;
-
-    public static void main(String[] args) throws IOException {
-
+class Main{
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         arr = new int[M];
-        dfs(1, 0);
-        System.out.println(sb);
+        dfs(N, M, 1, 0);
 
+        System.out.println(sb);
     }
-    public static void dfs(int at, int depth) {
-        if (depth == M) {
-            for (int val : arr) {
-                sb.append(val).append(' ');
+
+    public static void dfs(int N, int M, int num, int depth){
+        if(depth == M){
+            for(int i : arr){
+                sb.append(i).append(' ');
             }
             sb.append('\n');
             return;
         }
 
-        for (int i = at; i <= N; i++) {
+        for(int i = num; i<=N; i++){
             arr[depth] = i;
-            dfs(i + 1, depth + 1);
+            dfs(N, M, i+1, depth+1);
         }
     }
+
 }
