93c510fe75233c62e5fcd4adc0956261202ae23b
seunggyu <seunggyu97@gmail.com>
2022-12-25 20:02:07 +0900

    12/25 문제풀이 메뤼 크리스마스~~~

---
+++ src/Java/Programmers/Lv1/PR_연습문제_문자열나누기.java
@@ -0,0 +1,34 @@
+package Java.Programmers.Lv1.SplitStringArray;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+
+        while (s.length() > 0) {
+            answer++;
+            char ch = s.charAt(0);
+            int sCount = 1;
+            int dCount = 0;
+            for (int i = 1; i < s.length(); i++) {
+                if (ch == s.charAt(i))
+                    sCount++;
+                else
+                    dCount++;
+
+                if (sCount == dCount)
+                    break;
+            }
+
+            s = s.substring(sCount + dCount);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "aaabbaccccabba";
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(s));
+    }
+}
